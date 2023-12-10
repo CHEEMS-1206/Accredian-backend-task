16f9870f39b1202b0a1bcae38f53d4f58c5e6273
PRIYANSHU SINGH <sentysingh456@gmail.com>
2023-12-10 21:48:43 +0530

    defined models for user details for further login and registering users

---
+++ models/users.js
@@ -0,0 +1,43 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/config.js";
+
+const UserDetails = sequelize.define("userDetails", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      len: [8], // Password should strictly be 8 characters long
+    },
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true,
+    },
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW,
+    allowNull: false,
+  },
+  updatedAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW,
+    allowNull: false,
+  },
+});
+
+export default UserDetails;
